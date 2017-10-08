835088110ac0fceb23284b83b168f77f7d70ecb4
sciential <sciential@163.com>
2017-10-08 17:16:02 +0800

    try 4 times

--- src/com/youmeek/demo/HelloWorld.java
+++ src/com/youmeek/demo/HelloWorld.java
@@ -3,5 +3,6 @@ package com.youmeek.demo;
 public class HelloWorld {
     public static void main(String[] args){
         System.out.println("Hello World");
+        System.out.println("Hello World 2");
     }
 }
